3441c88a85defe6d2246ad8f95a98798ae73dde7
Andrew Monshizadeh <amonshiz@thebrowser.company>
2023-03-29 21:29:55 -0400

    Correct Package.swift, references to OHHTTPStubs, and deprecated method names

--- Sources/OpenGraph/Extension/URLSession.swift
+++ Sources/OpenGraph/Extension/URLSession.swift
@@ -32,25 +32,22 @@ public extension URLSession {
     func data(for request: URLRequest) async throws -> (Data, URLResponse) {
         var dataTask: URLSessionDataTask?
         let onCancel = { dataTask?.cancel() }
-        
-        return try await withTaskCancellationHandler(
-            handler: {
-                onCancel()
-            },
-            operation: {
-                try await withCheckedThrowingContinuation { continuation in
-                    dataTask = self.dataTask(with: request) { data, response, error in
-                        guard let data = data, let response = response else {
-                            let error = error ?? URLError(.badServerResponse)
-                            return continuation.resume(throwing: error)
-                        }
-                        
-                        continuation.resume(returning: (data, response))
+
+        return try await withTaskCancellationHandler(operation: {
+            try await withCheckedThrowingContinuation { continuation in
+                dataTask = self.dataTask(with: request) { data, response, error in
+                    guard let data = data, let response = response else {
+                        let error = error ?? URLError(.badServerResponse)
+                        return continuation.resume(throwing: error)
                     }
-                    
-                    dataTask?.resume()
+
+                    continuation.resume(returning: (data, response))
                 }
+
+                dataTask?.resume()
             }
-        )
+        }, onCancel: {
+            onCancel()
+        })
     }
 }
